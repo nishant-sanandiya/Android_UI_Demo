74a98aadb5e5a3b07ce4243437736ef692056da6
nishant.sanandiya <nishant.sanandiya@agileinfoways.com>
2023-04-13 14:08:19 +0530

    add glide and implemnt in ui

--- app/src/main/java/com/example/ui_demo/UserAdapter.kt
+++ app/src/main/java/com/example/ui_demo/UserAdapter.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.ui_demo.ApiModels.Data
 
 class UserAdapter(private val dataSet: List<Data>, private val context: Context) :
@@ -17,6 +19,7 @@ class UserAdapter(private val dataSet: List<Data>, private val context: Context)
         val firstNameView: TextView = view.findViewById(R.id.user_first_name_text)
         val lastNameView: TextView = view.findViewById(R.id.user_last_name_text)
         val emailView: TextView = view.findViewById(R.id.user_email_text)
+        val userImageView: ImageView = view.findViewById(R.id.userImage)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
@@ -34,6 +37,12 @@ class UserAdapter(private val dataSet: List<Data>, private val context: Context)
         viewHolder.firstNameView.text = dataSet[position].firstName.toString()
         viewHolder.lastNameView.text = dataSet[position].lastName.toString()
         viewHolder.emailView.text = dataSet[position].email.toString()
+        Glide
+            .with(this.context)
+            .load(dataSet[position].avatar)
+            .fitCenter()
+            .placeholder(R.drawable.image1)
+            .into(viewHolder.userImageView);
     }
 
     override fun getItemCount() = dataSet.size
